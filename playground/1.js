3f62551b0c93ff45e97b32c7782a3fafa985c734
makkvim <92586263+makkvim@users.noreply.github.com>
2021-10-15 18:47:48 +0530

    Update 1.js

--- playground/1.js
+++ playground/1.js
@@ -5,7 +5,6 @@ const api = require('novelcovid');
 api.settings({
     baseUrl: 'https://disease.sh' | 'https://api.caw.sh' | 'https://corona.lmao.ninja'
 })
-
 // this prints a summary of global data
 const data  = async ()=>{ 
     return await api.all().then()
