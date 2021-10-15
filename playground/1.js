c3750a2d1cc378f7df3080e40687a9df54318ebb
Rahul Kumar Parjapati <64469353+Rahulkprajapati@users.noreply.github.com>
2021-10-15 18:40:02 +0530

    Update 1.js

--- playground/1.js
+++ playground/1.js
@@ -6,7 +6,7 @@ api.settings({
     baseUrl: 'https://disease.sh' | 'https://api.caw.sh' | 'https://corona.lmao.ninja'
 })
 
-// this prints a summary of global data
+// this prints a summary of global data!!
 const data  = async ()=>{ 
     return await api.all().then()
 }
